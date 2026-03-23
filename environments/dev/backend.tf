53971c1a2964c2a1411596a2e21476ba53210cca
topshipw <topshipw@gmail.com>
2026-03-23 16:41:37 +0900

    init start2

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-project-terraform-state-683668078297-team03"
+    bucket         = "my-project-terraform-state-570263066900-team03"
     key            = "dev/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
